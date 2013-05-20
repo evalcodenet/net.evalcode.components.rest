509d2774dad62150a4d92a6207ecb9b6f2c126d1
Carsten Schipke <carsten.schipke@gmail.com>
2013-05-20 21:34:50 +0800

    * [common] Fixed exception namespaces. * [type] Added super-type Resource and refactored uri, urn, resource/type to a unified API for resource identification & handling. * [persistence] Added skeletons for SQL/NoSQL abstractions, utilities & object (relational) mapping.

--- rest/source/resource.php
+++ rest/source/resource.php
@@ -36,22 +36,22 @@ namespace Components;
     {
       $resource=get_called_class();
       if(!($method=$uri_->shiftPathParam()))
-        throw new Http_Exception('components/rest/resource', Http_Exception::NOT_FOUND);
+        throw new Http_Exception('rest/resource', Http_Exception::NOT_FOUND);
 
       if(null===self::$m_methods || false===isset(self::$m_methods[$resource][$method]))
       {
         self::initializeMethods();
 
         if(false===isset(self::$m_methods[$resource][$method]))
-          throw new Http_Exception('components/rest/resource', Http_Exception::NOT_FOUND);
+          throw new Http_Exception('rest/resource', Http_Exception::NOT_FOUND);
       }
 
       $method=self::$m_methods[$resource][$method];
       if(false===isset($method['methods'][$context_->getRequest()->getMethod()]))
-        throw new Http_Exception('components/rest/resource', Http_Exception::NOT_FOUND);
+        throw new Http_Exception('rest/resource', Http_Exception::NOT_FOUND);
 
       if(isset($method['path']) && count($uri_->getPathParams())<count($method['path']))
-        throw new Http_Exception('components/rest/resource', Http_Exception::NOT_FOUND);
+        throw new Http_Exception('rest/resource', Http_Exception::NOT_FOUND);
 
       /* @var $resource Components\Rest_Resource */
       $resource=new $resource();
